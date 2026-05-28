0ed7442f8cd8e153cf2c989a41cae7840172b5e3
DarkMod101 <unfathomed101@gmail.com>
2026-05-27 21:42:46 -0400

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,32 @@
+const CACHE_NAME = "ascended-masters-pushup-tracker-v1";
+
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./app.js",
+  "./manifest.json",
+  "./assets/icons/icon-192.png",
+  "./assets/icons/icon-512.png",
+  "./assets/images/standard-pushup.png",
+  "./assets/images/finger-root-pushup.png",
+  "./assets/images/diamond-pushup.png",
+  "./assets/images/decline-pushup.png",
+  "./assets/images/incline-pushup.png",
+  "./assets/images/explosive-pushup.png",
+  "./assets/images/headstand-pushup.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(cached => {
+      return cached || fetch(event.request);
+    })
+  );
+});
